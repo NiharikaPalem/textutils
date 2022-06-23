c4536e3e32c338eb9b03e5ff08ed6ddf09c640eb
NiharikaPalem <niharikapalem25@gmail.com>
2022-06-23 14:34:36 +0530

    Updates

---
+++ precache-manifest.f613b7ceae30ec48a60f21d1f498388c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf5129b4ddadf3bcbfdc",
+    "url": "/textutils/static/css/main.d7b874e7.chunk.css"
+  },
+  {
+    "revision": "cf5129b4ddadf3bcbfdc",
+    "url": "/textutils/static/js/main.cf5129b4.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "6f1764d27b9504d6bef96fead7c295b0",
+    "url": "/textutils/index.html"
+  }
+];
